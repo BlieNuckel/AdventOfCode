ed5f975f579313e04c12cf428a398e5ca89dc9a6
BuildTools <unconfigured@null.spigotmc.org>
2020-12-08 17:54:36 +0100

    ass

--- e2020/src/main/java/Day4.java
+++ e2020/src/main/java/Day4.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day4 extends Advent{
 
@@ -47,6 +49,44 @@ public class Day4 extends Advent{
 
     @Override
     protected Object solveSecond() {
-        return null;
+        int count = 0;
+        final HashSet<String> EYE_COLORS = new HashSet<>(List.of("amb", "blu", "brn", "gry", "grn", "hzl", "oth"));
+        final HashSet<String> REQUIRED_FIELDS = new HashSet<>(List.of("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"));
+        for (Map<String, String> i : parsedId) {
+            int c = 0;
+            if (i.keySet().containsAll(REQUIRED_FIELDS)) {
+                if (Integer.parseInt(i.get("byr")) >= 1920 && Integer.parseInt(i.get("byr")) <= 2002) {
+                    c++;
+                }
+                if (Integer.parseInt(i.get("iyr")) >= 2010 && Integer.parseInt(i.get("iyr")) <= 2020) {
+                    c++;
+                }
+                if (Integer.parseInt(i.get("eyr")) >= 2020 && Integer.parseInt(i.get("eyr")) <= 2030) {
+                    c++;
+                }
+                if (i.get("hgt").startsWith("cm", i.get("hgt").length() - 2)) {
+                    if (Integer.parseInt(i.get("hgt").split("[^0-9]+")[0]) >= 150 && Integer.parseInt(i.get("hgt").split("[^0-9]+")[0]) <= 193) {
+                        c++;
+                    }
+                } else if (i.get("hgt").startsWith("in", i.get("hgt").length() - 2)) {
+                    if (Integer.parseInt(i.get("hgt").split("[^0-9]+")[0]) >= 59 && Integer.parseInt(i.get("hgt").split("[^0-9]+")[0]) <= 76) {
+                        c++;
+                    }
+                }
+                if (Pattern.matches("#\\p{XDigit}{6}", i.get("hcl"))) {
+                    c++;
+                }
+                if (EYE_COLORS.contains(i.get("ecl"))) {
+                    c++;
+                }
+                if (Pattern.matches("\\d{9}", i.get("pid"))) {
+                    c++;
+                }
+            }
+            if (c == 7) {
+                count++;
+            }
+        }
+        return count;
     }
 }
