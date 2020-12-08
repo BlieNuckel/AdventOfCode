4e15ec3becdc53e133344f1100e360dc1ac28a08
BuildTools <unconfigured@null.spigotmc.org>
2020-12-08 22:14:47 +0100

    ass

--- e2020/src/main/java/Day5.java
+++ e2020/src/main/java/Day5.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.HashSet;
 
 public class Day5 extends Advent{
 
@@ -10,18 +11,58 @@ public class Day5 extends Advent{
         super(day);
     }
 
+    HashSet<Double> parsed;
+
     @Override
     protected void setup() {
-
+        parsed = new HashSet<>();
+        HashSet<String> tempSet = new HashSet<>(getInput());
+        for (String i : tempSet) {
+            double rowLow = 0;
+            double rowHigh = 127;
+            double row = 0;
+            for (int j = 0; j < 7; j++) {
+                if (i.charAt(j) == 'B') {
+                    if (rowLow == rowHigh - 1) row = rowHigh;
+                    else rowLow += Math.ceil((rowHigh - rowLow)/2);
+                } else if (i.charAt(j) == 'F') {
+                    if (rowLow == rowHigh - 1) row = rowLow;
+                    else rowHigh -= Math.ceil((rowHigh - rowLow)/2);
+                }
+            }
+            double colLow = 0;
+            double colHigh = 7;
+            double col = 0;
+            for (int j = 7; j < 10; j++) {
+                if (i.charAt(j) == 'R') {
+                    if (colLow == colHigh - 1) col = colHigh;
+                    else colLow += Math.ceil((colHigh - colLow)/2);
+                } else if (i.charAt(j) == 'L') {
+                    if (colLow == colHigh - 1) col = colLow;
+                    else colHigh -= Math.ceil((colHigh - colLow)/2);
+                }
+            }
+            parsed.add(row * 8 + col);
+        }
     }
 
     @Override
     protected Object solveFirst() {
-        return null;
+        double highest = 0;
+        for (Double i : parsed) {
+            if (i > highest) highest = i;
+        }
+        return highest;
     }
 
     @Override
     protected Object solveSecond() {
-        return null;
+        double num = 0;
+        for (double i = 0; i < 996; i++) {
+            if (!parsed.contains(i)) {
+                num = i;
+            }
+        }
+        return num;
     }
 }
