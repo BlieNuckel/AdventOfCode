54a152076328e7e485f9ecc770b71818c9597cfa
BuildTools <unconfigured@null.spigotmc.org>
2020-12-08 18:11:49 +0100

    ass

---
+++ e2020/src/main/java/Day5.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+public class Day5 extends Advent{
+
+    public static void main(String[] args) throws IOException {
+        new Day5(5);
+    }
+
+    protected Day5(int day) throws IOException {
+        super(day);
+    }
+
+    @Override
+    protected void setup() {
+
+    }
+
+    @Override
+    protected Object solveFirst() {
+        return null;
+    }
+
+    @Override
+    protected Object solveSecond() {
+        return null;
+    }
+}
